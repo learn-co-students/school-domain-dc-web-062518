c04bbf08d1c5028e623604bafd4ae3a47da66a9a
Andres Manco <andresmanco@gmail.com>
2018-06-27 19:03:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+
+class School
+  def initialize(name)
+    @school_name = name
+    @roster = {}
+  end
+  
+  def roster 
+    @roster
+  end
+  
+  def add_student(name, grade)
+    @studen_name = name
+    @grade = grade
+    if @roster == {}
+      @roster[grade] = [name]
+    else
+      found = false
+      @roster.each do |g, n|
+        if g == grade
+          @roster[g] << name
+          found = true
+        end
+      end
+      if found == false
+        @roster[grade] = [name]
+      end
+    end
+    
+  end
+end
